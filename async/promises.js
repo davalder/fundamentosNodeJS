bf955ce0d8b426319e89c71adc442404c059d90b
davalder <davalder@hotmail.com>
2021-05-08 17:23:14 -0500

    clase9 Promesas anidadas

--- async/promises.js
+++ async/promises.js
@@ -1,21 +1,41 @@
-//Funciones en configuracion recursiva para eliminar administrar callbackHell
-function hola(nombre, miCallback) {
-    setTimeout(function() {
-        console.log('Hola, ' + nombre);
-        miCallback(nombre);
-    }, 1000)
+//Promesas anidadas
+function hola(nombre) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            console.log('Hola, ' + nombre);
+            resolve(nombre);
+        }, 1000);
+    });
 }
 
-function hablar(callbackHablar) {
-    setTimeout(function () {
-        console.log('bla bla bla');
-        callbackHablar();
-    }, 1000)
+function hablar(nombre) {
+    return new Promise( (resolve, reject) => {
+        setTimeout(function () {
+            console.log('bla bla bla');
+            resolve();
+        }, 1000)
+    });
 }
 
-function adios(nombre, otroCallback) {
-    setTimeout(function() {
-        console.log('Adios ' + nombre);
-        otroCallback();
-    }, 1000);
+function adios(nombre) {
+    return new Promise( (resolve, reject) => {
+        setTimeout(function() {
+            console.log('Adios ' + nombre);
+            resolve();
+        }, 1000);
+    });
 }
+
+// --
+
+console.log('Iniciando el proceso...');
+hola('Diego')
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)
+    .then((nombre) => {
+        console.log('Terminado el proceso');
+    })
