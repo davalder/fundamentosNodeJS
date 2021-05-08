2ebd8ffee2c6263fcf20c88b18c5ea08d19ef141
davalder <davalder@hotmail.com>
2021-05-08 17:10:27 -0500

    se crea archivo clase9 promises.js

---
+++ async/promises.js
@@ -0,0 +1,21 @@
+//Funciones en configuracion recursiva para eliminar administrar callbackHell
+function hola(nombre, miCallback) {
+    setTimeout(function() {
+        console.log('Hola, ' + nombre);
+        miCallback(nombre);
+    }, 1000)
+}
+
+function hablar(callbackHablar) {
+    setTimeout(function () {
+        console.log('bla bla bla');
+        callbackHablar();
+    }, 1000)
+}
+
+function adios(nombre, otroCallback) {
+    setTimeout(function() {
+        console.log('Adios ' + nombre);
+        otroCallback();
+    }, 1000);
+}
