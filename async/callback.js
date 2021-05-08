9fbb9b5b3953f3c9e51352d27d0a1c5333fec880
davalder <davalder@hotmail.com>
2021-05-08 16:14:35 -0500

    clase 7 Funciones comportandose sincronas con Callback

--- async/callback.js
+++ async/callback.js
@@ -1,12 +1,21 @@
-//Funcion comportandose sincrona con Callback
-function soyAsincrona(miCallback) {
+//Funciones comportandose sincronas con Callback
+function hola(nombre, miCallback) {
     setTimeout(function() {
-        console.log('Estoy siendo asíncrono');
-        miCallback();
+        console.log('Hola, ' + nombre);
+        miCallback(nombre);
     }, 1000)
 }
 
+function adios(nombre, otroCallback) {
+    setTimeout(function() {
+        console.log('Adios ' + nombre);
+        otroCallback();
+    }, 1000);
+}
+
 console.log('Iniciando proceso....')
-soyAsincrona(function() {
-    console.log('Terminando proceso....');
+hola('Diego', function(nombre) {
+    adios(nombre, function() {
+        console.log('Terminando proceso....');
+    });
 });
