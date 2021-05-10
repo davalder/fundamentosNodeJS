fb1bdf29c6f1f26863a2e398bbb1dcbc17414e3b
davalder <davalder@hotmail.com>
2021-05-10 16:26:49 -0500

    clase 14 try/catch. Captura del error en funcion ejecutada por otra funcion en sincrono

--- modulos/errores.js
+++ modulos/errores.js
@@ -1,10 +1,17 @@
 // clase 14 try/catch. Captura del error de la funcion se rompe
+
+
+// Captura del error en funcion ejecutada por otra funcion en sincrono
+function otraFuncion() {
+    serompe();
+}
+
 function serompe() {
     return 3 + z;
 }
 
 try{
-    serompe();
+    otraFuncion();
 } catch(err) {
     console.error('Vaya, also se ha roto')
     console.error(err.message);
