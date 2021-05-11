55fd3b8d1fc6df568206734ce0e8c80449bc2b6a
davalder <davalder@hotmail.com>
2021-05-11 13:01:47 -0500

    clase19, clase terminada

--- modulos/process.js
+++ modulos/process.js
@@ -9,5 +9,26 @@ p.on('beforeExit', () => {
 
 p.on('exit', () => {
     console.log('Diego, el proceso acabó');
+    setTimeout(() => {
+        console.log('Esto no se va a ver');
+    }, 0)
 });
+
+setTimeout(() => {
+        console.log('Esto si se va a ver');
+    }, 0);
+
+// excepciones que no se han capturado
+p.on('uncaughtException', (err, origen) => {
+    console.error('Se nos olvido capturar el error');
+    //console.error(err);
+    setTimeout(() => {
+        console.log('Esto viene desde las excepciones');
+    }, 0);
+} )
+
+funcionquenoexiste();
+
+//Promesas que se han rechazado
+
 
