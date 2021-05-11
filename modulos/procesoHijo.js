acbda9cf4723459b14efeed26e478e45d4f65873
davalder <davalder@hotmail.com>
2021-05-11 10:31:56 -0500

    clse15, ejecutar un proceso que esta en otro archivo, ejemplo la clase consola.

--- modulos/procesoHijo.js
+++ modulos/procesoHijo.js
@@ -2,7 +2,18 @@
 const { exec } = require('child_process'); // ó const exec = require('child_process').exec;
 
 //consultar el directorio, en linux se usa 'ls -la' y en window 'dir'
-exec('ls -la', (err, stdout, sterr) => {
+// exec('ls -la', (err, stdout, sterr) => {
+//     if (err) {
+//         console.error(err);
+//         return false;
+//     }
+
+//     console.log(stdout);
+// })
+
+// ejecutar un proceso que esta en otro archivo, ejemplo la clase consola.
+
+exec('node modulos/consola.js', (err, stdout, sterr) => {
     if (err) {
         console.error(err);
         return false;
