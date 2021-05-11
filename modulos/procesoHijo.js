923c0ef367ab31aeb80fd458ce0f9dcc9466e43e
davalder <davalder@hotmail.com>
2021-05-11 10:42:30 -0500

    clase15, invocar un proceso nuevo en nodeJS con spawn

--- modulos/procesoHijo.js
+++ modulos/procesoHijo.js
@@ -1,5 +1,5 @@
 // se trae parte de la libreria child_process
-const { exec } = require('child_process'); // ó const exec = require('child_process').exec;
+const { exec, spawn } = require('child_process'); // ó const exec = require('child_process').exec;
 
 //consultar el directorio, en linux se usa 'ls -la' y en window 'dir'
 // exec('ls -la', (err, stdout, sterr) => {
@@ -13,11 +13,30 @@ const { exec } = require('child_process'); // ó const exec = require('child_pro
 
 // ejecutar un proceso que esta en otro archivo, ejemplo la clase consola.
 
-exec('node modulos/consola.js', (err, stdout, sterr) => {
-    if (err) {
-        console.error(err);
-        return false;
-    }
+// exec('node modulos/consola.js', (err, stdout, sterr) => {
+//     if (err) {
+//         console.error(err);
+//         return false;
+//     }
 
-    console.log(stdout);
+//     console.log(stdout);
+// })
+
+// invocar un proceso nuevo en nodeJS con spawn
+
+let proceso = spawn('ls', ['-la']);
+
+console.log(proceso.pid);
+console.log(proceso.connected);
+
+proceso.stdout.on('data', function(dato) {
+    console.log('Esta muerto el proceso?');
+    console.log(proceso.killed);
+    console.log(dato.toString());
+});
+
+proceso.on('exit', function() {
+    console.log('el proceso termino');
+    console.log('Esta muerto el proceso?');
+    console.log(proceso.killed);
 })
