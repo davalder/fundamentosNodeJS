ccc4b099b3c35f993b25a0d088aec810d4af72ef
davalder <davalder@hotmail.com>
2021-05-12 10:59:07 -0500

    clase22, utilizando bcrypt para comparar si es el password correcto con bcrypt.compare()

--- paquetes/utiles/bcrypt.js
+++ paquetes/utiles/bcrypt.js
@@ -2,10 +2,12 @@ const bcrypt = require('bcrypt');
 
 const password = '1234Segura!';
 
-
 //encripta el password
 bcrypt.hash(password, 5, function(err, hash) {
     console.log(hash);
-})
 
-bcrypt.compare
+    bcrypt.compare(password, hash, function(err, res) {
+        //console.log(err);
+        console.log(res);
+    })
+})
