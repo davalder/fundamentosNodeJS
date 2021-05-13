30ad896a838f17c993b7d35cc9a4625372f4b705
davalder <davalder@hotmail.com>
2021-05-13 10:23:44 -0500

    clase25, como hacer cuando el archivo es muy grande

--- memoria/stream.js
+++ memoria/stream.js
@@ -10,5 +10,12 @@ let readableStream = fs.createReadStream(__dirname + '/input.txt');
  //se lee el stream, el cual toma la info del archivo input.txt
 readableStream.setEncoding('UTF8'); // se define el tipo de codigo, para este caso es texto y se usa el estandar UTF8
  readableStream.on('data', function (chunk) {
-    console.log(chunk);
+    data += chunk; //si el archivo solicitado es grande y viene por partes se añade a data parte por parte
+});
+
+//ademas de saber que es grande el archivo, se necesita saber cuando termina
+
+readableStream.on('end', function() {
+    //aqui hacemos lo que deseamos con el archivo cuando ya se completo la traida.
+    console.log(data);
 })
