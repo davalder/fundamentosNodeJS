90a92d345a3df25512fd4508d26797709921d538
davalder <davalder@hotmail.com>
2021-05-13 09:13:11 -0500

    clase24, array, cada item es un espacio, que aloja el dato escrito

--- memoria/buffer.js
+++ memoria/buffer.js
@@ -1,4 +1,4 @@
 // crear buffer
-let buffer = Buffer.alloc(4); // primer parametro es el tamaño del buffer, 1 un espacio, 4 cuatro espacios.
-
+//let buffer = Buffer.alloc(4); // primer parametro es el tamaño del buffer, 1 un espacio, 4 cuatro espacios.
+let buffer = Buffer.from([1, 2, 5]); // array, cada item es un espacio, que aloja el dato escrito
 console.log(buffer);
