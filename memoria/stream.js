774e6757eff2524486810400d6ed2d44ad33a61c
davalder <davalder@hotmail.com>
2021-05-13 10:16:53 -0500

    clase25, se define el tipo de codigo, para este caso es texto y se usa el estandar UTF8

--- memoria/stream.js
+++ memoria/stream.js
@@ -8,6 +8,7 @@ let data = '';
 let readableStream = fs.createReadStream(__dirname + '/input.txt');
 
  //se lee el stream, el cual toma la info del archivo input.txt
-readableStream.on('data', function (chunk) {
+readableStream.setEncoding('UTF8'); // se define el tipo de codigo, para este caso es texto y se usa el estandar UTF8
+ readableStream.on('data', function (chunk) {
     console.log(chunk);
 })
