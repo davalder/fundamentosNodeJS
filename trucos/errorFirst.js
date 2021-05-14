105a55ae9e3d402a5e633959a161ce3eebb722bf
davalder <davalder@hotmail.com>
2021-05-14 14:56:43 -0500

    clase28, capturando errores en funciones asincronas

---
+++ trucos/errorFirst.js
@@ -0,0 +1,23 @@
+// capturando errores en funciones asincronas
+
+function asincrona(callback) {
+    setTimeout(function() {
+        try {
+            let a = 3 + z;
+            callback(null, a);
+        } catch (e) {
+            callback(e);
+        }
+    }, 1000)
+}
+
+asincrona(function(err, dato) {
+    if (err) {
+        console.error('Tenemos un error');
+        console.error(err);
+        return false;
+        //throw err; // NO FUNCIONA EN ASINCRONAS
+    }
+
+    console.log('Todo funciono bien mi data es', dato)
+})
