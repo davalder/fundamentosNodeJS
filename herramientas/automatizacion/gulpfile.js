214f7b24df1a0f581ccb150f20aca4dadb38f224
davalder <davalder@hotmail.com>
2021-05-16 14:08:34 -0500

    clase30, para crear servidor // tenemos problemas por a version de node, debe ser menor a la 14

--- herramientas/automatizacion/gulpfile.js
+++ herramientas/automatizacion/gulpfile.js
@@ -1,8 +1,19 @@
 const gulp = require('gulp');
+const server = require('gulp-server-livereload');
 
 
 // se contruye tarea para inicar con gulp, usando el script build, creado en package.json
 gulp.task('build', function(cb) {
     console.log('Construyendo el sitio');
     setTimeout(cb, 1200);
+});
+
+
+//para crear servidor // tenemos problemas por a version de node, debe ser menor a la 14
+gulp.task('serve', function(cb) {
+    gulp.src('www')
+        .pipe(server({
+            livereload: true,
+            open: true,
+        }));
 })
